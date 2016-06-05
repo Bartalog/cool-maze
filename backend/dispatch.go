ec387496358d7d31545eead2b7d4837cef9c7003
Vde <deleplace2015@gmail.com>
2016-06-05 23:46:06 +0200

    Backend basic params validation.

--- backend/dispatch.go
+++ backend/dispatch.go
@@ -1,6 +1,10 @@
 package coolmaze
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 import "github.com/pusher/pusher-http-go"
 import "appengine"
 import "appengine/urlfetch"
@@ -10,16 +14,38 @@ func init() {
 }
 
 func dispatch(w http.ResponseWriter, r *http.Request) {
-	// TODO: allow POST only.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Only POST method is accepted")
+		return
+	}
+
 	channelID := r.FormValue("chanID")
 	event := "maze-cast"
 	message := r.FormValue("message")
+
+	if channelID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Mandatory parameter: chanID")
+		return
+	}
+	if message == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Mandatory parameter: message")
+		return
+	}
+	if _, err := strconv.Atoi(channelID); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "channelID must be an integer")
+		return
+	}
 
 	c := appengine.NewContext(r)
 	urlfetchClient := urlfetch.Client(c)
 	c.Infof("Sending to chan [%v] message [%v]", channelID, message)
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	client := pusher.Client{
 		AppId:      "197093",
 		Key:        "e36002cfca53e4619c15",
