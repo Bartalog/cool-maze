9ce334f7781dffb047753f78aec64b55c165de63
Deleplace <deleplace2015@gmail.com>
2016-07-27 20:22:12 +0200

    issues/22 /wakeup endpoint.

---
+++ backend/wakeup.go
@@ -0,0 +1,20 @@
+package coolmaze
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	//
+	// Wake Up is a sort of manual (explicit) warmup request.
+	// It should be triggered by all clients at startup (browser, mobile device) to
+	// make sure the subsequent request won't face a cold start (Loading request).
+	// See also https://cloud.google.com/appengine/docs/go/warmup-requests/
+	//
+	http.HandleFunc("/wakeup",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "ok")
+			// Instance should be warm by now
+		})
+}
