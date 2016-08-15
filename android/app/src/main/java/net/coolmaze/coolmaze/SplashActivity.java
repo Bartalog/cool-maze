567bfbe236a120c1b6bebfa6f60e3cf2eae7cb35
Vde <deleplace2015@gmail.com>
2016-08-15 19:40:28 +0200

    issues/28 Onboarding activity with nice pic.

---
+++ android/app/src/main/java/net/coolmaze/coolmaze/SplashActivity.java
@@ -0,0 +1,33 @@
+package net.coolmaze.coolmaze;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        closeCountdown(4);
+    }
+
+    void closeCountdown(int seconds) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 1000 * seconds);
+    }
+}
