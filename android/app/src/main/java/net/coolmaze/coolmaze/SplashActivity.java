00e4ec2385f1a0237ec0b0e7862c5f99582393d9
Vde <deleplace2015@gmail.com>
2016-08-19 22:24:10 +0200

    issues/45 Add sample button, on splash screen.

--- android/app/src/main/java/net/coolmaze/coolmaze/SplashActivity.java
+++ android/app/src/main/java/net/coolmaze/coolmaze/SplashActivity.java
@@ -1,15 +1,20 @@
 package net.coolmaze.coolmaze;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
+
+    final String samplePhotoURL = "https://storage.googleapis.com/cool-maze.appspot.com/sample.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,15 @@ public class SplashActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        closeCountdown(4);
+        Button button_sample = (Button) findViewById(R.id.button_sample);
+        button_sample.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchSampleSend();
+            }
+        });
+
+        //closeCountdown(4);
     }
 
     void closeCountdown(int seconds) {
@@ -29,5 +42,24 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, 1000 * seconds);
+    }
+
+    void launchSampleSend(){
+        new AlertDialog.Builder(this)
+                .setMessage("Please choose to share via \"Cool Maze\"")
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setPositiveButton("Got it", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        Intent intent = new Intent();
+                        //Intent intent = new Intent(this, MainActivity.class);
+                        intent.setAction(Intent.ACTION_SEND);
+                        intent.setType("text/plain");
+                        intent.putExtra(Intent.EXTRA_TEXT, samplePhotoURL);
+
+                        startActivity(Intent.createChooser(intent, "Share via"));
+                    }
+                })
+                .show();
     }
 }
